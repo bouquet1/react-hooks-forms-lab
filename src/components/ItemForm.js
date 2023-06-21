2fecc7d54a71219023e56bcfb6f82c7542a63a06
bouquet1 <bukuleta@gmail.com>
2023-06-20 23:27:22 -0700

    search bar works Filter comp done

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,6 +1,52 @@
-import React from "react";
+import React, { useState } from "react"; //don't forget to {useState}
 import { v4 as uuid } from "uuid";
 
+function ItemForm({ onItemFormSubmit }) {
+  const [itemCategory, setItemCategory] = useState("Produce");
+  const [itemName, setItemName] = useState([]);
+
+  //handleSubmit function to get the info user create, newItem object.
+  function handleSubmit(e) {
+    e.preventDefault();
+    const newItem = {
+      id: uuid(),
+      name: itemName,
+      category: itemCategory, //set state for itemName and itemCategory to get the info that user and to display them as a newItem at the list
+    };
+    onItemFormSubmit(newItem);
+  }
+
+  /*add onChange to <input> and get the value. Burda onChange={(e) => setItemName(e.target.value)} seklinde cb function i direk inline yazabilirim. Ya da ayirabilirim soyle:
+  onChange={handleAddNewItemName} ve de ayrica 
+    function handleAddNewItemName(e){
+      setItemName
+    } same thing for the category */
+
+  return (
+    <form className="NewItem" onSubmit={handleSubmit}>
+      <label>
+        Name:
+        <input type="text" name="name" onChange={(e) => setItemName(e.target.value)} value={itemName} />
+      </label>
+
+      <label>
+        Category:
+        <select name="category" value={itemCategory} onChange={(e) => setItemCategory(e.target.value)}>
+          <option value="Produce">Produce</option>
+          <option value="Dairy">Dairy</option>
+          <option value="Dessert">Dessert</option>
+        </select>
+      </label>
+
+      <button type="submit">Add to List</button>
+    </form>
+  );
+}
+
+export default ItemForm;
+
+/*
+beginning code 
 function ItemForm(props) {
   return (
     <form className="NewItem">
@@ -24,3 +70,4 @@ function ItemForm(props) {
 }
 
 export default ItemForm;
+*/
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -3,25 +3,45 @@ import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
 
-function ShoppingList({ items }) {
+function ShoppingList({ items, setItems }) {
+  //pass items and setItems from App. Use them to display the initial list of items and to update the list when a new item is added.
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [search, setSearch] = useState("");
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
 
+  function handleSearchBar(e) {
+    setSearch(e.target.value);
+  }
+
   const itemsToDisplay = items.filter((item) => {
     if (selectedCategory === "All") return true;
-
     return item.category === selectedCategory;
   });
 
+  const filteredItems = itemsToDisplay.filter((item) => item.name.toLowerCase().includes(search.toLowerCase()));
+
+  //create onItemFormSubmit as a callback and pass it as a prop to ItemForm
+  function onItemFormSubmit(newItem) {
+    setItems(...items, newItem); //create a new array with spread operator -nondestructive-
+  }
+
   return (
     <div className="ShoppingList">
-      <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <ItemForm onItemFormSubmit={onItemFormSubmit} />
+      <Filter
+        onCategoryChange={handleCategoryChange}
+        onSearchChange={handleSearchBar}
+        search={search}
+        selectedCategory={selectedCategory}
+      />
       <ul className="Items">
-        {itemsToDisplay.map((item) => (
+        {/* {itemsToDisplay.map((item) => (
+          <Item key={item.id} name={item.name} category={item.category} />
+        ))} */}
+        {filteredItems.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
@@ -30,3 +50,22 @@ function ShoppingList({ items }) {
 }
 
 export default ShoppingList;
+
+/*
+  This way worked too. Second alternative to write this code
+  const itemsToDisplay = items.filter((item) => {
+    if (search !== "") {
+      return item.name.toLowerCase().includes(search.toLowerCase());
+    } else {
+      if (selectedCategory === "All") return true;
+
+      return item.category === selectedCategory;
+    }
+  });
+
+  This code have the rest part as an uncommented part in JSX above. itemsToDisplay.map() part.
+  
+  {itemsToDisplay.map((item) => (
+          <Item key={item.id} name={item.name} category={item.category} />
+        ))} 
+  */
