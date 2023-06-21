b2304835118d497c3d3db3905756568fbd8c8c1f
bouquet1 <bukuleta@gmail.com>
2023-06-21 00:03:27 -0700

    finally passed test []

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -44,30 +44,3 @@ function ItemForm({ onItemFormSubmit }) {
 }
 
 export default ItemForm;
-
-/*
-beginning code 
-function ItemForm(props) {
-  return (
-    <form className="NewItem">
-      <label>
-        Name:
-        <input type="text" name="name" />
-      </label>
-
-      <label>
-        Category:
-        <select name="category">
-          <option value="Produce">Produce</option>
-          <option value="Dairy">Dairy</option>
-          <option value="Dessert">Dessert</option>
-        </select>
-      </label>
-
-      <button type="submit">Add to List</button>
-    </form>
-  );
-}
-
-export default ItemForm;
-*/
